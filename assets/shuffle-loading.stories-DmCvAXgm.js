f351c1ad41dda12f285abc735da2d12098d4fe3d
minoong <minoong@users.noreply.github.com>
2026-01-25 01:48:56 +0000

    deploy: 706e7999b1b0eb9015ae326540f265045eb907d0

--- assets/shuffle-loading.stories-DmCvAXgm.js
+++
@@ -1,5 +0,0 @@
-import{S as a}from"./shuffle-loading-D3n_HTNT.js";import"./iframe-D4KFpum-.js";import"./preload-helper-PPVm8Dsz.js";import"./feature-bundle-2XgFgHG-.js";const m={title:"Shared/Loading/ShuffleLoading",component:a,parameters:{layout:"centered"},tags:["autodocs"]},e={args:{size:"medium"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    size: 'medium'
-  }
-}`,...e.parameters?.docs?.source}}};const n=["Default"];export{e as Default,n as __namedExportsOrder,m as default};
